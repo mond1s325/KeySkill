c1784406464ccb08b83f2966c9ee45a7f3df43ef
mond1s325 <187334808+mond1s325@users.noreply.github.com>
2025-07-10 01:30:14 +0200

    Delete animated_logo.py

--- animated_logo.py
+++
@@ -1,9 +0,0 @@
-from manim import *
-
-class Logo(Scene):
-    def construct(self):
-        text = Text("KeySkill", font_size=72, color=WHITE)
-        self.play(
-                Write(text,run_time=3)
-            ) 
-        
